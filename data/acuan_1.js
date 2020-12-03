e236f55ddee932b7ca7b7a887ed13f571ee1e985
septiansw <75385568+septiansw@users.noreply.github.com>
2020-12-03 13:50:24 +0700

    Add files via upload

---
+++ data/acuan_1.js
@@ -0,0 +1,13 @@
+var json_acuan_1 = {
+"type": "FeatureCollection",
+"name": "acuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 111.413023333380849, -6.969110733428592 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 111.425163481700181, -6.966816659010465 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 111.417539362296139, -6.974068005315018 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 111.415361042466387, -6.965709170791177 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 111.421869437079593, -6.970745584208329 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 111.411057532558871, -6.973356059918571 ] } }
+]
+}
